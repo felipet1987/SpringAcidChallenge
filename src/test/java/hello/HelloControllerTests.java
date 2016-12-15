09b5a45b0824ca3266111e3ce4b3d1a1a55e5dbb
Your Name <you@example.com>
2016-12-15 20:51:37 -0300

    arreglo en test

--- src/test/java/hello/HelloControllerTests.java
+++ src/test/java/hello/HelloControllerTests.java
@@ -43,19 +43,7 @@ public class HelloControllerTests {
 	    } 
     	
 
-    @Test
-    public void noParamGreetingShouldReturnDefaultMessage() throws Exception {
 
-        this.mockMvc.perform(get("/Hello")).andDo(print()).andExpect(status().isOk())
-                .andExpect(jsonPath("$.content").value("Hello World!"));
-    }
-//
-//    @Test
-//    public void paramGreetingShouldReturnTailoredMessage() throws Exception {
-//
-//        this.mockMvc.perform(get("/greeting").param("name", "Spring Community"))
-//                .andDo(print()).andExpect(status().isOk())
-//                .andExpect(jsonPath("$.content").value("Hello, Spring Community!"));
-//    }
+
 
 }
