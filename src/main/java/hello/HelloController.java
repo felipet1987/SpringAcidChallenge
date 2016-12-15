49cb8ad0f6fb41be405abac8a1b7d032a9166755
Your Name <you@example.com>
2016-12-15 20:47:16 -0300

    primer commit

---
+++ src/main/java/hello/HelloController.java
@@ -0,0 +1,18 @@
+package hello;
+
+import java.util.concurrent.atomic.AtomicLong;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+
+
+
+    @RequestMapping("/Hello")
+    public String Hello(@RequestParam(value="name", defaultValue="World") String name) {
+		return  "Hello " + name;
+   	
+    }
+}
